aafbf7c2564dc4a7ca125758baff9cca8792b672
Nguyen Thi Ngoc Thom <11203791@st.neu.edu.vn>
2022-06-24 23:36:59 +0700

    thom dang sua

--- QLTV/Frmlogin.Designer.cs
+++ QLTV/Frmlogin.Designer.cs
@@ -56,6 +56,7 @@ namespace QLTV
             this.button2.TabIndex = 1;
             this.button2.Text = "Đăng nhập";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
--- QLTV/Frmlogin.cs
+++ QLTV/Frmlogin.cs
@@ -16,5 +16,10 @@ namespace QLTV
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ///////hello
+        }
     }
 }
